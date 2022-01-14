2bc8e9e6eab7a21c64945d3de8c48c3c96d567f4
Bonjur <55990897+MrBonjur@users.noreply.github.com>
2022-01-14 20:25:45 +0300

    Update example.py

--- example.py
+++ example.py
@@ -14,6 +14,3 @@ byte_array = MemoryScanner.get_array_of_bytes(address=0x7FF77422FC82, len_bytes=
 text = MemoryScanner.convert_array_to_text(b"\xB1\x00\x01")
 array = MemoryScanner.convert_text_to_array("B1 00 01")
 hex_to_text = MemoryScanner.get_hex_to_text("45 72 72 6F 72 49 6E 66 6F", "utf-8")
-
-
-
